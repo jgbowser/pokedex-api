d60a9edb5824a7679dffb302b5b05ba4f76b9257
John Bowser <john.bow7720@gmail.com>
2020-08-10 22:59:59 -0600

    added dotenv for token handling

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 
@@ -5,9 +6,19 @@ const app = express();
 
 app.use(morgan('dev'));
 
-app.use((req, res) => {
-  res.send('Hello, World!');
-});
+const validTypes = ['Bug', 'Dark', 'Dragon', 'Electric', 'Fairy', 'Fighting', 'Fire', 'Flying', 'Ghost', 'Grass', 'Ground', 'Ice', 'Normal', 'Poison', 'Psychic', 'Rock', 'Steel', 'Water']
+
+function handleGetTypes(req, res) {
+  res.json(validTypes);
+}
+
+app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req, res) {
+  res.send('Hello, Pokemon!');
+}
+
+app.get('/pokemon', handleGetPokemon);
 
 const PORT = 8000;
 
